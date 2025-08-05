14fd86d6f31810c5896743ebbb9a9b3d587fdb35
Mahghuuuls <arthcrs@gmail.com>
2025-08-05 20:21:21 -0300

    Initial commit.

---
+++ src/main/java/com/mahghuuuls/ctsetbonus/CTSetBonus.java
@@ -0,0 +1,14 @@
+package com.mahghuuuls.ctsetbonus;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = CTSetBonus.MOD_ID, name = CTSetBonus.NAME, version = CTSetBonus.VERSION)
+public class CTSetBonus {
+	public static final String MOD_ID = "ctsetbonus";
+	public static final String NAME = "CTSetBonus";
+	public static final String VERSION = "0.1.0";
+	public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
+}
---
+++ src/main/java/com/mahghuuuls/ctsetbonus/IPlayerExpansions.java
@@ -0,0 +1,34 @@
+package com.mahghuuuls.ctsetbonus;
+
+import com.fantasticsource.setbonus.SetBonusData;
+import com.fantasticsource.setbonus.common.Bonus;
+import com.fantasticsource.setbonus.server.ServerBonus;
+
+import crafttweaker.annotations.ZenRegister;
+import crafttweaker.api.minecraft.CraftTweakerMC;
+import crafttweaker.api.player.IPlayer;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.EntityPlayerMP;
+import stanhebben.zenscript.annotations.ZenExpansion;
+import stanhebben.zenscript.annotations.ZenMethod;
+
+@ZenExpansion("crafttweaker.player.IPlayer")
+@ZenRegister
+public class IPlayerExpansions {
+
+	@ZenMethod
+	public static boolean hasSetBonus(IPlayer iPlayer, String bonusID) {
+		EntityPlayer player = CraftTweakerMC.getPlayer(iPlayer);
+		if (player.world.isRemote)
+			return false;
+		EntityPlayerMP playerMP = (EntityPlayerMP) player;
+
+		for (Bonus bonus : SetBonusData.SERVER_DATA.bonuses) {
+			if (!bonusID.equals(bonus.id))
+				continue;
+			ServerBonus serverBonus = (ServerBonus) bonus;
+			return serverBonus.getBonusInstance(playerMP).active;
+		}
+		return false;
+	}
+}
