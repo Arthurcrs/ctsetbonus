aedf16a321cc1ae41db19ea87d0dbd6adc81eb89
Mahghuuuls <arthcrs@gmail.com>
2025-08-14 22:54:12 -0300

    Moved the dependencies string to a static variable in the main class (to match the others)

--- src/main/java/com/mahghuuuls/ctsetbonus/CTSetBonus.java
+++ src/main/java/com/mahghuuuls/ctsetbonus/CTSetBonus.java
@@ -6,11 +6,12 @@ import org.apache.logging.log4j.Logger;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
 
-@Mod(modid = CTSetBonus.MOD_ID, name = CTSetBonus.NAME, version = CTSetBonus.VERSION, dependencies = "required-after:crafttweaker;required-after:setbonus")
+@Mod(modid = CTSetBonus.MOD_ID, name = CTSetBonus.NAME, version = CTSetBonus.VERSION, dependencies = CTSetBonus.DEPENDENCIES)
 public class CTSetBonus {
 	public static final String MOD_ID = "ctsetbonus";
 	public static final String NAME = "CTSetBonus";
 	public static final String VERSION = "0.2.0";
+	public static final String DEPENDENCIES = "required-after:crafttweaker;required-after:setbonus";
 	public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
 
 	@Mod.EventHandler
